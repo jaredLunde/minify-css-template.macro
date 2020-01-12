ab4023b8c9f5e8cf249d7016a9345982b9f6a0b7
Jared Lunde <jared.lunde@gmail.com>
2020-01-12 15:26:40 -0700

    Makes placeholders type more broad

--- types/macro.d.ts
+++ types/macro.d.ts
@@ -1,4 +1,4 @@
 declare module 'minify-css.macro' {
-  const minifyCss: (literals: TemplateStringsArray, ...placeholders: string[]) => string
+  const minifyCss: (literals: TemplateStringsArray, ...placeholders: any[]) => string
   export default minifyCss
 }
